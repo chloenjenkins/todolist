c6d1de787f0acb60a666ec21f5314c304df8a050
chloenjenkins <102946243+chloenjenkins@users.noreply.github.com>
2022-04-04 19:54:49 +0800

    added event listener

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let addTaskButton = document.getElementById("add-task")
 let newTaskInput = document.getElementById("task-input")
 let todoListContainer = document.getElementById("todo-list")
+
 let templateElement = document.getElementById ("list-item-template")
 let template = templateElement.innerHTML
 //step 2: write a function to implement the behaviour
@@ -12,5 +13,10 @@ function onAddTaskClicked(event){
     todoListContainer.insertAdjacentHTML("afterbegin", taskHTML);
 
 }
+function onTodoListClicked(event){
+    let targetElement = event.toElement;
+    console.log(targetElement);
+}
 //step 3: link element, function and event
 addTaskButton.addEventListener('click', onAddTaskClicked)
+todoListContainer.addEventListener('click', onTodoListClicked)
