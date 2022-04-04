713a7f8f000466574590cd45dc8b13a4b0b2504f
chloenjenkins <102946243+chloenjenkins@users.noreply.github.com>
2022-04-04 20:10:33 +0800

    fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // step 1: find elements
-let addTaskButton = document.getElementById("add-task")
-let newTaskInput = document.getElementById("task-input")
-let todoListContainer = document.getElementById("todo-list")
+let addTaskButton = document.getElementById("add-task");
+let newTaskInput = document.getElementById("task-input");
+let todoListContainer = document.getElementById("todo-list");
 
 let templateElement = document.getElementById ("list-item-template")
 let template = templateElement.innerHTML
@@ -9,14 +9,27 @@ let template = templateElement.innerHTML
 function onAddTaskClicked(event){
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
+    
     let taskHTML = template.replace("<!--task_name-->", taskName);
     todoListContainer.insertAdjacentHTML("afterbegin", taskHTML);
 
 }
 function onTodoListClicked(event){
     let targetElement = event.toElement;
-    console.log(targetElement);
+    
+    while(!targetElement.classList.contains("task")){
+        targetElement = targetElement.parentElement
+    }
+    let checkbox = targetElement.querySelector(".checkbox");
+
+    if (checkbox.checked){
+        targetElement.classList.add("completed");
+    }
+    else
+    {
+        targetElement.classList.remove("completed");
+    }
 }
 //step 3: link element, function and event
-addTaskButton.addEventListener('click', onAddTaskClicked);
-todoListContainer.addEventListener('click', onTodoListClicked);
+addTaskButton.addEventListener('click',onAddTaskClicked);
+todoListContainer.addEventListener('click',onTodoListClicked);
