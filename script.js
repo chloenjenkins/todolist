4980c17aa9a56aa682b0cf5c860e9448c45a3276
chloenjenkins <102946243+chloenjenkins@users.noreply.github.com>
2022-04-04 20:14:56 +0800

    fixed

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ let addTaskButton = document.getElementById("add-task");
 let newTaskInput = document.getElementById("task-input");
 let todoListContainer = document.getElementById("todo-list");
 
-let templateElement = document.getElementById ("list-item-template")
-let template = templateElement.innerHTML
+let templateElement = document.getElementById ("list-item-template");
+let template = templateElement.innerHTML;
 //step 2: write a function to implement the behaviour
-function onAddTaskClicked(event){
+function onAddTaskClicked(event) {
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
     
     let taskHTML = template.replace("<!--task_name-->", taskName);
-    todoListContainer.insertAdjacentHTML("afterbegin", taskHTML);
+    todoListContainer.insertAdjacentHTML("beforeend", taskHTML);
 
 }
 function onTodoListClicked(event){
-    let targetElement = event.toElement;
+    let targetElement = event.target;
     
     while(!targetElement.classList.contains("task")){
-        targetElement = targetElement.parentElement
+        targetElement = targetElement.parentElement;
     }
     let checkbox = targetElement.querySelector(".checkbox");
 
